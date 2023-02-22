e933716d1270e35dfd749adc6159e615ae2c4867
NickyBlackburn <nickblackburn02@gmail.com>
2023-02-21 21:55:36 -0500

    working on logo screen

---
+++ src/main/java/starblazerstudio/screens/LoadingScreen.java
@@ -0,0 +1,110 @@
+package starblazerstudio.screens;
+
+/***
+ * this class is for changing the images on the loading screen
+ */
+
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
+import net.minecraft.client.gui.screens.LoadingOverlay;
+import net.minecraft.resources.ResourceLocation;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+
+import org.lwjgl.system.Platform;
+
+public class LoadingScreen {
+
+    // adding loading image loading screen
+    public String setLoadingImage() {
+
+        String filelocal = "";
+
+        // ALLOWS ME TO Swich the loading image based on day
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+
+        switch (calendar.get(Calendar.DAY_OF_WEEK)) {
+            case 1:
+                filelocal = "blackburn/title/logo.png";
+
+                break;
+
+            case 2:
+                filelocal = "blackburn/title/logo2.png";
+                break;
+
+            case 3:
+                filelocal = "blackburn/title/logo3.png";
+                break;
+
+            case 4:
+                filelocal = "blackburn/title/logo4.png";
+                break;
+
+            case 5:
+                filelocal = "blackburn/title/logo5.png";
+                break;
+
+            case 6:
+                filelocal = "blackburn/title/logo6.png";
+                break;
+
+            case 7:
+                filelocal = "blackburn/title/logo7.png";
+                break;
+
+            default:
+                filelocal = "blackburn/title/logo.png";
+                break;
+        }
+        return filelocal;
+
+    }
+
+    // sets the default mojang logo local
+    public void setLogoDefaultLogoLocal(PoseStack pose, GuiComponent component, int j2, int j1, int k2, int i1, int d1) {
+   
+        component.blit(pose, j2 - j1, k2 - i1, j1, (int) d1, -0.0625F, 0.0F, 120, 60, 120, 120);
+        component.blit(pose, j2, k2 - i1, j1, (int) d1, 0.0625F, 60.0F, 120, 60, 120, 120);
+
+    }
+
+    // seting small logo pos
+    public void setCustomLogoPosNonFull(LoadingOverlay component, PoseStack pMatrixStack, int px, int py, int UOffset,
+            float VOffest, int pWidth, int pHight, int pTextureWidth, int pTextureHight, int screenWitdh,int j){
+                
+        int width = 0 ;
+    if(px < 0 ){
+        width =  screenWitdh / 2 -  Math.abs(px);
+       }else{
+        width = screenWitdh / 2 + px;
+       }
+
+       int y = 0;
+   
+       if(py < 0){
+          y = j+72 - Math.abs(py);
+       } else{
+          y = j+72 + py;
+       }
+   
+        component.blit(pMatrixStack, width, y, UOffset, VOffest, pWidth, pHight, pTextureWidth, pTextureHight);
+
+    }
+
+    // seting logo pos
+    public void setCustomLogoPosFull(LoadingOverlay component, PoseStack pMatrixStack, int windowWidth,int px, int py, int UOffset,
+            float VOffest, int pWidth, int pHight, int pTextureWidth, int pTextureHight) {
+
+        component.blit(pMatrixStack, px, py, UOffset, VOffest, pWidth, pHight, pTextureWidth, pTextureHight);
+
+    }
+    
+    
+    
+}
---
+++ src/main/java/starblazerstudio/utils/Consts.java
@@ -0,0 +1,150 @@
+package starblazerstudio.utils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.level.block.Block;
+
+
+
+public class Consts {
+    
+
+    public static List background = new ArrayList<Integer>();
+    public static List keys = new ArrayList<Integer>();
+
+    public static List commands = new LinkedList<String>();
+    public static List commanddesc = new LinkedList<String>();
+
+
+    public static List blocklistseen = new LinkedList<Block>();
+    public static List blocklistseenpos = new LinkedList<Double>();
+
+ // Tewitch chat
+    public static List chatmessage = new LinkedList<String>();
+    public static List chatuser = new LinkedList<String>();
+    public static List finalmessage = new LinkedList<String>();
+
+    public static Minecraft minecraft  = Minecraft.getInstance();
+
+    
+    public static boolean devMode = false;
+    public static boolean showStart = true;
+    public static boolean crit = false;
+    public static boolean nofall = false;
+
+
+    public static  boolean enableTwitch = false;
+    
+    private static final org.apache.logging.log4j.Logger LOGGER = LogManager.getLogger();
+   
+    // this is the logo dimention on the loading screen
+    public static int px =  -50;
+    public static int py = -100;
+    public static int  pTextureWidth = 120;
+    public static int pTextureHeight= 120;
+
+    public static int pWidth =  120;
+    public static int pHight = 120;
+
+    public static int pUOffset = 0 ;
+    public static float pVOffset = 0;
+
+    // Ufull
+    public static int pxfull =  190;
+    public static int pyfull = 80;
+    public static int  pTextureWidthfull = 120;
+    public static int pTextureHeightfull = 120;
+
+    public static int pWidthfull =  120;
+    public static int pHightfull = 120;
+
+    public static int pUOffsetfull = 0 ;
+    public static float pVOffsetfull = 0;
+
+    public static String RELEASE = "DEBUG";
+    public static String VERSION = "§c1§6.§e1§a8§9.§b1§5-§cB§6l§ea§ac§9k§bb§5u§cr§6n§r";
+    public static String Date = null;
+
+    public static String TwitchCurrentMessage;
+    public static String TwitchCurrentSent;
+
+    public static String TwitchUsername; 
+    public static String TwitchPass;
+
+    public static String TwitchConnected= "blackburn.twitch.connected.false";
+
+    public static String ReleaseName = "§cB§6l§ea§ac§9k§bb§5u§cr§6n§e'§as§r";
+
+
+    // CopyRight Text
+    public static String copyright = "§cC§6o§ep§ay§9R§bi§5g§ch§6t§ee§ad §bB§5y §6N§ei§ac§9k§by §cB§6l§ea§ac§9k§bb§5u§cr§6n";
+
+
+    public static boolean enablePosInfo = false;
+    public static boolean enableLightInfo = false;
+    public static boolean speed = false;
+
+
+    public static List twitchlog = new LinkedList<>();
+
+    public boolean getEnablePos(){
+        return enablePosInfo;
+    }
+
+
+    // commands stuff
+
+    public static String commandprefix = "command.blackburn.";
+
+
+    public static void dbg(String s)
+    {
+        LOGGER.info("[Blackburn] " + s);
+    }
+
+    public static void warn(String s)
+    {
+        LOGGER.warn("[Blackburn] " + s);
+    }
+
+    public static void warn(String s, Throwable t)
+    {
+        LOGGER.warn("[Blackburn] " + s, t);
+    }
+
+    public static void error(String s)
+    {
+        LOGGER.error("[Blackburn] " + s);
+    }
+
+    public static void error(String s, Throwable t)
+    {
+        LOGGER.error("[Blackburn] " + s, t);
+    }
+
+    public static void log(String s)
+    {
+        dbg(s);
+    }
+
+
+    public static void info(String s)
+    {
+        dbg(s);
+    }
+
+    public static void debug(String s)
+    {
+        dbg(s);
+    }
+
+ 
+
+
+
+}
