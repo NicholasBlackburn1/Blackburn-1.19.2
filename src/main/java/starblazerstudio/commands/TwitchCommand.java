5b33fa3cbeb2de622e2d682b1e21e18134f7804c
nicky blackburn <nickblackburn02@gmail.com>
2024-05-27 15:18:41 -0400

    owo nice tryting to get stuff working:

--- src/main/java/starblazerstudio/commands/TwitchCommand.java
+++ src/main/java/starblazerstudio/commands/TwitchCommand.java
@@ -30,13 +30,16 @@ public class TwitchCommand implements ICommandRegister{
            
                 }if(Consts.TwitchConnected.contains(I18n.a("blackburn.twitch.connected.true"))){
 
-                    
+                    mc.gui.getChat().clearMessages(true);
+
                     mc.gui.getChat().addMessage(Component.translatable(I18n.a("blackburn.commands.twitch.pre.col")).append(" > "+"is connected? "+I18n.a("blackburn.twitch.connected.true")));
                     mc.gui.getChat().addMessage(Component.translatable(I18n.a("blackburn.commands.twitch.connectacctrue")));
                     command.clear();
 
 
                 }else{
+                    mc.gui.getChat().clearMessages(true);
+                 
                     mc.gui.getChat().addMessage(Component.translatable(I18n.a("blackburn.commands.twitch.pre.col")).append(" > "+"is connected? "+I18n.a("blackburn.twitch.connected.false")));
                     mc.gui.getChat().addMessage(Component.translatable(I18n.a("blackburn.commands.twitch.connectaccfail")));
                     command.clear();
