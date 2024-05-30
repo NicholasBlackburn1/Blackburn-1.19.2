51646af02d3753117a786e167f3b199ae2da1cd9
nicky blackburn <nickblackburn02@gmail.com>
2024-05-30 13:29:12 -0400

    working on starting ot set up to dev lewd verifiyer

---
+++ src/main/java/starblazerstudio/utils/StatusVerifiyer.java
@@ -0,0 +1,19 @@
+/***
+ * 
+ * this is the verifyer for the status of the users uuid (to see if they can access the freinds only  content)
+ */
+
+package starblazerstudio.utils;
+
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+public class StatusVerifiyer {
+
+
+    public void veryifiyUser(){
+        
+    }
+    
+}
