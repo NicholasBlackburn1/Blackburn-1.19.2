d8d933003168444ce72c7d088409235d185d9db6
nicky blackburn <nickblackburn02@gmail.com>
2024-05-30 13:32:46 -0400

    added the verifyer base command stufdf

--- src/main/java/starblazerstudio/utils/Consts.java
+++ src/main/java/starblazerstudio/utils/Consts.java
@@ -90,6 +90,10 @@ public class Consts {
     public static boolean speed = false;
 
 
+    // verifyer code
+    public static String  homeurl = "127.0.0.1";
+    public static String  verifiyerurl = "http://"+homeurl+"/user_status";
+
     public static List twitchlog = new LinkedList<>();
 
     public boolean getEnablePos(){
