a96a5c22e11e755a226707575ef40ca3ce1dcce5
nicky blackburn <nickblackburn02@gmail.com>
2024-06-03 13:42:55 -0400

    working on setting uip bug screen

--- src/main/java/starblazerstudio/utils/key.java
+++
@@ -1,6 +0,0 @@
-package starblazerstudio.utils;
-
-public class key {
-    public static String githubkey = "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";
-
-}
