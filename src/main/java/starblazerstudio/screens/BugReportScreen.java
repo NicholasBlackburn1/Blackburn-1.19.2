532dc8fa888197dd4a27d8c08d1c5daeb2a9d6cb
nicky blackburn <nickblackburn02@gmail.com>
2024-06-08 13:05:10 -0400

    working on getting bug mennu working better

--- src/main/java/starblazerstudio/screens/BugReportScreen.java
+++ src/main/java/starblazerstudio/screens/BugReportScreen.java
@@ -1,25 +1,22 @@
 package starblazerstudio.screens;
 
 import java.io.IOException;
-
 import com.mojang.blaze3d.vertex.PoseStack;
-
 import it.unimi.dsi.fastutil.booleans.BooleanConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.Checkbox;
 import net.minecraft.client.gui.components.EditBox;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
 import starblazerstudio.guicomponents.GuiDropdownList;
 import starblazerstudio.utils.Consts;
 import starblazerstudio.utils.GitHubIssueCreator;
 import starblazerstudio.utils.IssueLabel;
 import starblazerstudio.utils.key;
-
-import java.util.LinkedList; // Import LinkedList from java.util
+import java.util.LinkedList;
 
 public class BugReportScreen extends Screen {
 
@@ -27,29 +24,28 @@ public class BugReportScreen extends Screen {
     private static final Component BODY_COMPONENT = Component.translatable("blackburn.bugmenu.body");
     private Button addButton;
     private final BooleanConsumer callback;
-    
-
     private EditBox title;
     private EditBox body;
-    private Checkbox isConnected;
     private final Screen lastScreen;
     private GitHubIssueCreator issueCreator;
     private String titleinput = "";
     private String bodyinput = "";
-
     private GuiDropdownList dropdownList;
- 
     private final Screen laast;
-
     private static String selected_label = "";
 
-    public BugReportScreen(Screen last) {
+    private ImageButton statusButton;
+    private static final ResourceLocation BUTTON_TEXTURE_DEFAULT = new ResourceLocation("modid", "textures/gui/button_default.png");
+    private static final ResourceLocation BUTTON_TEXTURE_SUCCESS = new ResourceLocation("modid", "textures/gui/button_success.png");
+    private static final ResourceLocation BUTTON_TEXTURE_FAIL = new ResourceLocation("modid", "textures/gui/button_fail.png");
+    private boolean isSuccess;
 
+    public BugReportScreen(Screen last) {
         super(Component.translatable(I18n.a("blackburn.bugmenu.screentitle")));
         this.callback = null;
         this.lastScreen = last;
         this.laast = last;
-
+        this.isSuccess = false;
     }
 
     public void tick() {
@@ -57,77 +53,62 @@ public class BugReportScreen extends Screen {
         this.body.tick();
     }
 
-   
     protected void init() {
-
-        // sets up the Title box
         this.minecraft.keyboardHandler.setSendRepeatsToGui(true);
-        this.title = new EditBox(this.font, this.width / 2 - 100, 66, 200, 20,
-                Component.translatable("addServer.enterName"));
+        this.title = new EditBox(this.font, this.width / 2 - 100, 66, 200, 20, Component.translatable("addServer.enterName"));
         this.title.setFocus(true);
         this.title.setValue(this.title.getValue());
         this.title.setMaxLength(255);
-
         this.title.setResponder((p_169304_) -> {
             titleinput = this.title.getValue();
         });
-
         this.addWidget(this.title);
 
-        // creates the witch pass box
-        this.body = new EditBox(this.font, this.width / 2 - 100, 106, 200, 20,
-                Component.translatable("blackburn.twitch.password"));
+        this.body = new EditBox(this.font, this.width / 2 - 100, 106, 200, 20, Component.translatable("blackburn.twitch.password"));
         this.body.setMaxLength(255);
         this.body.setValue(this.body.getValue());
-
         this.body.setResponder((p_169302_) -> {
             bodyinput = this.body.getValue();
         });
         this.addWidget(this.body);
 
-        /**
-         * isssue scellection
-         */
         LinkedList<String[]> labelOptions = new LinkedList<>();
-        labelOptions.add(new String[] { "Select Tag", "" }); // Add an empty option
+        labelOptions.add(new String[]{"Select Tag", ""});
         for (IssueLabel label : IssueLabel.values()) {
-            labelOptions.add(new String[] { label.toString(), label.toString() });
+            labelOptions.add(new String[]{label.toString(), label.toString()});
         }
+        this.addRenderableWidget(dropdownList = new GuiDropdownList(this.width / 2 - 100, this.height / 4 + 80, 50, 20, labelOptions.toArray(new String[0][]), (button) -> {}));
 
-        this.addRenderableWidget(dropdownList = new GuiDropdownList(this.width / 2 - 100, this.height / 4 + 80, 50, 20,
-                labelOptions.toArray(new String[0][]), (button) -> {
-                  
-                    
-                }));
+        this.statusButton = new ImageButton(this.width / 2 + 60, this.height / 4 + 80, 50, 20, 0, 0, BUTTON_TEXTURE_DEFAULT, button -> {}, Component.empty());
+        this.addRenderableWidget(this.statusButton);
 
+        this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 4 + 96 + 18, 200, 20, Component.translatable("blackburn.bugmenu.connect"), (p_96030_) -> {
+            issueCreator = new GitHubIssueCreator(Consts.repouser, Consts.reponame, key.githubkey);
+            Consts.error("Selected label: " + Consts.bugmenulabel);
+            Consts.isconnected = true;
 
-        // shows that u summited the stuff succwssfully
-        this.addRenderableWidget(new Checkbox(this.width / 2 + 60, this.height / 4 + 80, 50, 20, Component.translatable("blackburn.bugmenu.isconnected"),Consts.isconnected ));
+            try {
+                issueCreator.createIssue(titleinput, bodyinput, Consts.bugmenulabel);
+                isSuccess = true;
+                updateStatusButtonTexture();
+            } catch (Exception e) {
+                e.printStackTrace();
+                isSuccess = false;
+                updateStatusButtonTexture();
+            }
+        }));
 
-        // connection button
-        this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 4 + 96 + 18, 200, 20,
-                Component.translatable("blackburn.bugmenu.connect"), (p_96030_) -> {
+        this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 4 + 120 + 18, 200, 20, CommonComponents.GUI_CANCEL, (p_169297_) -> {
+            onClose();
+        }));
+    }
 
-                    issueCreator = new GitHubIssueCreator(Consts.repouser, Consts.reponame, key.githubkey);
-
-                        Consts.error("Selected label: " + Consts.bugmenulabel);
-                        Consts.isconnected = true;
-
-                    try {
-                        issueCreator.createIssue(titleinput, bodyinput, Consts.bugmenulabel);
-                        
-                        
-                    } catch (Exception e) {
-                        e.printStackTrace(); // Log the exception stack trace
-                    }
-
-
-                }));
-
-        this.addRenderableWidget(new Button(this.width / 2 - 100, this.height / 4 + 120 + 18, 200, 20,
-                CommonComponents.GUI_CANCEL, (p_169297_) -> {
-                    onClose();
-                }));
+    private void updateStatusButtonTexture() {
+        if (isSuccess) {
+            this.statusButton.setResourceLocation(BUTTON_TEXTURE_SUCCESS);
+        } else {
+            this.statusButton.setResourceLocation(BUTTON_TEXTURE_FAIL);
+        }
     }
 
     public void resize(Minecraft pMinecraft, int pWidth, int pHeight) {
@@ -138,8 +119,6 @@ public class BugReportScreen extends Screen {
         this.title.setValue(s1);
     }
 
-
-    
     public void removed() {
         this.minecraft.keyboardHandler.setSendRepeatsToGui(false);
     }
@@ -148,19 +127,12 @@ public class BugReportScreen extends Screen {
         this.minecraft.setScreen(this.lastScreen);
     }
 
-    private void updateAddButtonStatus() {
-        // Consts.twitchconnector.setupBot();
-    }
-
     public void render(PoseStack pPoseStack, int pMouseX, int pMouseY, float pPartialTick) {
         this.renderBackground(pPoseStack);
-
         drawString(pPoseStack, this.font, TITLE_COMPONENT, this.width / 2 - 100, 53, 10526880);
         drawString(pPoseStack, this.font, BODY_COMPONENT, this.width / 2 - 100, 94, 10526880);
         this.title.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
         this.body.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
         super.render(pPoseStack, pMouseX, pMouseY, pPartialTick);
     }
-
-    
 }
