a88eeb4133f47e29b04d9d5f4085c93cce758171
nicky blackburn <nickblackburn02@gmail.com>
2024-06-09 18:15:12 -0400

    trying to workon getting stuff working

--- src/main/java/starblazerstudio/screens/ClientSettingsScreen.java
+++ src/main/java/starblazerstudio/screens/ClientSettingsScreen.java
@@ -53,7 +53,7 @@ public class ClientSettingsScreen extends Screen
         }));
         
          // the twtich button 
-         this.addRenderableWidget(new Button(this.width / 2 + 5, this.height / 6 + 48 + 6, 150, 20, Component.translatable("blackburn.bugmenu.title"), (p_96274_) ->
+         this.addRenderableWidget(new Button(this.width / 2 + 5, this.height / 6 + 48 + 20, 150, 20, Component.translatable("blackburn.bugmenu.title"), (p_96274_) ->
          {
              this.minecraft.setScreen(new BugReportScreen(this));
          }));
